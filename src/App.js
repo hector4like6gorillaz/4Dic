5cd87a7fcf4b3a9dc29b706fa85948c621d7bf37
hectorbalan <balan.huchin.3@gmail.com>
2021-12-04 11:44:31 -0600

    botones negros

--- src/App.js
+++ src/App.js
@@ -94,27 +94,63 @@ function App() {
       {/* calculadora */}
       <div className="calcu">
         <div className="pantalla">
-          <h1>987654231</h1>
+          <h1 className="nums">987654231</h1>
         </div>
         <div className="botones">
-          <div className="uno"><button className="tecla">1</button></div>
-          <div className="dos"><button className="tecla">2</button></div>
-          <div className="tres"><button className="tecla">3</button></div>
-          <div className="cuatro"><button className="tecla">4</button></div>
-          <div className="cinco"><button className="tecla">5</button></div>
-          <div className="seis"><button className="tecla">6</button></div>
-          <div className="siete"><button className="tecla">7</button></div>
-          <div className="ocho"><button className="tecla">8</button></div>
-          <div className="nueve"><button className="tecla">9</button></div>
-          <div className="cero"><button className="tecla">0</button></div>
-          <div className="sum"><button className="tecla">+</button></div>
-          <div className="res"><button className="tecla">-</button></div>
-          <div className="mul"><button className="tecla">x</button></div>
-          <div className="divi"><button className="tecla">/</button></div>
-          <div className="reset"><button className="tecla">res</button></div>
-          <div className="igual"><button className="tecla">=</button></div>
-          <div className="del"><button className="tecla">{`<-`}</button></div>
-          <div className="punto"><button className="tecla">.</button></div>
+          <div className="uno marbtn">
+            <button className="tecla">1</button>
+          </div>
+          <div className="dos marbtn">
+            <button className="tecla">2</button>
+          </div>
+          <div className="tres marbtn">
+            <button className="tecla">3</button>
+          </div>
+          <div className="cuatro marbtn">
+            <button className="tecla">4</button>
+          </div>
+          <div className="cinco marbtn">
+            <button className="tecla">5</button>
+          </div>
+          <div className="seis marbtn">
+            <button className="tecla">6</button>
+          </div>
+          <div className="siete marbtn">
+            <button className="tecla">7</button>
+          </div>
+          <div className="ocho marbtn">
+            <button className="tecla">8</button>
+          </div>
+          <div className="nueve marbtn">
+            <button className="tecla">9</button>
+          </div>
+          <div className="cero marbtn">
+            <button className="tecla">0</button>
+          </div>
+          <div className="sum marbtn">
+            <button className="tecla">+</button>
+          </div>
+          <div className="res marbtn">
+            <button className="tecla">-</button>
+          </div>
+          <div className="mul marbtn">
+            <button className="tecla">x</button>
+          </div>
+          <div className="divi marbtn">
+            <button className="tecla">/</button>
+          </div>
+          <div className="reset marbtn">
+            <button className="tecla">res</button>
+          </div>
+          <div className="igual marbtn">
+            <button className="tecla">=</button>
+          </div>
+          <div className="del marbtn">
+            <button className="tecla">{`<-`}</button>
+          </div>
+          <div className="punto marbtn">
+            <button className="tecla">.</button>
+          </div>
         </div>
       </div>
 
